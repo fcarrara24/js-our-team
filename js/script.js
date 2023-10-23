a9a01b8d058b40e20299cc08c43e64a0f209be8e
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-10-23 15:33:15 +0200

    MILESTONE 1: copied the array with all data

---
+++ js/script.js
@@ -0,0 +1,39 @@
+"use strict"
+
+
+
+//array containeing all registered users
+const employee = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        picture: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        picture: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        picture: "walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manafewr",
+        picture: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        picture: "scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        picture: "barbara-ramos-graphic-designer.jpg",
+    },
+
+
+];
